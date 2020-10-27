7dbc90210f9156238051245f228623f2a044f30c
Norbert Benkó <norbert.benko@infinitaslearning.com>
2020-10-27 13:48:49 +0100

    now takes inputs also from arguments

--- ZipComparer/Program.cs
+++ ZipComparer/Program.cs
@@ -6,20 +6,28 @@ namespace DirectoryComparer
     {
         static void Main(string[] args)
         {
-            string[] inputs = TakeInputs();
+            string[] inputs = TakeInputs(args);
             DirectoryComparer dc = new DirectoryComparer(inputs[0], inputs[1]);
             dc.CompareDirectoriesFileCount();
             dc.CompareDirectoriesFileContent();
         }
 
-        private static string[] TakeInputs()
+        private static string[] TakeInputs(string[] args)
         {
             string[] inputs = new string[2];
 
-            Console.WriteLine("Old project directory's path:");
-            inputs[0] = @$"{Console.ReadLine()}";
-            Console.WriteLine("New project directory's path:");
-            inputs[1] = @$"{Console.ReadLine()}";
+            if (args.Length != 0)
+            {
+                inputs[0] = args[0];
+                inputs[1] = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Old project directory's path:");
+                inputs[0] = @$"{Console.ReadLine()}";
+                Console.WriteLine("New project directory's path:");
+                inputs[1] = @$"{Console.ReadLine()}";
+            }
 
             return inputs;
         }
