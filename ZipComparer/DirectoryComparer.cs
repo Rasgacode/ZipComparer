71794da2ed0892f9880616fa950b5442b5a318e6
Norbert Benkó <norbert.benko@infinitaslearning.com>
2020-10-27 13:20:32 +0100

    extend with directory comparer

---
+++ ZipComparer/DirectoryComparer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryComparer
+{
+    public class DirectoryComparer
+    {
+        private readonly string oldDirectoryPath;
+        private readonly string newDirecotryPath;
+        private readonly IEnumerable<string> oldProjectFiles;
+        private readonly IEnumerable<string> newProjectFiles;
+
+        public DirectoryComparer(string _oldDirectoryPath, string _newDirectoryPath)
+        {
+            oldDirectoryPath = CorrectDirectoryPath(_oldDirectoryPath);
+            newDirecotryPath = CorrectDirectoryPath(_newDirectoryPath);
+            oldProjectFiles = GetDirectoryFiles(oldDirectoryPath);
+            newProjectFiles = GetDirectoryFiles(newDirecotryPath);
+        }
+
+        public void CompareDirectoriesFileCount()
+        {
+            IEnumerable<string> missingFromNew = oldProjectFiles.Where(file => !newProjectFiles.Contains(file));
+            IEnumerable<string> missingFromOld = newProjectFiles.Where(file => !oldProjectFiles.Contains(file));
+
+            if (missingFromNew.Count() > 0)
+            {
+                Console.WriteLine("These files were in OldProjectDirectory but not in the NewProjectDirectory:");
+                foreach (string item in missingFromNew)
+                {
+                    Console.Write($"{item}, ");
+                }
+                Console.WriteLine();
+            }
+
+            if (missingFromOld.Count() > 0)
+            {
+                Console.WriteLine("These files were in NewProjectDirectory but not in the OldProjectDirectory:");
+                foreach (string item in missingFromOld)
+                {
+                    Console.Write($"{item}, ");
+                }
+                Console.WriteLine();
+            }
+
+            if (missingFromOld.Count() == 0 && missingFromNew.Count() == 0)
+            {
+                Console.WriteLine("The two directories contains the same files.");
+            }
+        }
+
+        public void CompareDirectoriesFileContent() 
+        {
+            bool success = true;
+            IEnumerable<string> matchingFiles = oldProjectFiles.Where(file => newProjectFiles.Contains(file));
+
+            foreach (string file in matchingFiles)
+            {
+                string oldProjectFilePath = oldDirectoryPath + file;
+                string newProjectFilePath = newDirecotryPath + file;
+                
+                if (!FileCompare(oldProjectFilePath, newProjectFilePath))
+                {
+                    Console.WriteLine($"{oldProjectFilePath} and {newProjectFilePath} was not the same file.");
+                    success = false;
+                }
+            }
+
+            if (success && matchingFiles.Count() > 0)
+            {
+                Console.WriteLine("All matching files (depend on filename) have the same content in the selected directories.");
+            }
+
+            if (matchingFiles.Count() == 0)
+            {
+                Console.WriteLine("There are no files in the two directories that are matching.");
+            }
+        }
+
+        private bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            if (file1 == file2)
+            {
+                return true;
+            }
+
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2, FileMode.Open);
+
+            if (fs1.Length != fs2.Length)
+            {
+                fs1.Close();
+                fs2.Close();
+
+                return false;
+            }
+
+            do
+            {
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            fs1.Close();
+            fs2.Close();
+
+            return ((file1byte - file2byte) == 0);
+        }
+
+        private IEnumerable<string> GetDirectoryFiles(string directoryPath)
+        {
+            try
+            {
+                return Directory.GetFiles(directoryPath).Select(file => file.Replace(directoryPath, ""));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Something is wrong with this path: {directoryPath}");
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        private string CorrectDirectoryPath(string directoryPath)
+        {
+            return directoryPath.Last() == '\\' ? directoryPath : directoryPath + @"\";
+        }
+    }
+}
--- ZipComparer/Program.cs
+++ ZipComparer/Program.cs
@@ -1,79 +1,27 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 
-namespace ZipComparer
+namespace DirectoryComparer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ZipCompare();
+            string[] inputs = TakeInputs();
+            DirectoryComparer dc = new DirectoryComparer(inputs[0], inputs[1]);
+            dc.CompareDirectoriesFileCount();
+            dc.CompareDirectoriesFileContent();
         }
 
-        private static void ZipCompare()
+        private static string[] TakeInputs()
         {
             string[] inputs = new string[2];
 
-            while (!inputs.Contains("exit"))
-            {
-                Console.WriteLine("First zip's path:");
-                inputs[0] = @$"{Console.ReadLine()}";
-                Console.WriteLine("Second zip's path:");
-                inputs[1] = @$"{Console.ReadLine()}";
-                try
-                {
-                    if (FileCompare(inputs))
-                    {
-                        Console.WriteLine("Files are equal.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Files are not equal.");
-                    }
-                }
-                catch (FileNotFoundException)
-                {
-                    Console.WriteLine("File was not found (wrong path)!");
-                }
+            Console.WriteLine("Old project directory's path:");
+            inputs[0] = @$"{Console.ReadLine()}";
+            Console.WriteLine("New project directory's path:");
+            inputs[1] = @$"{Console.ReadLine()}";
 
-            }
-        }
-
-        private static bool FileCompare(string[] inputs)
-        {
-            int file1byte;
-            int file2byte;
-            FileStream fs1;
-            FileStream fs2;
-
-            if (inputs[0] == inputs[1])
-            {
-                return true;
-            }
-
-            fs1 = new FileStream(inputs[0], FileMode.Open);
-            fs2 = new FileStream(inputs[1], FileMode.Open);
-
-            if (fs1.Length != fs2.Length)
-            {
-                fs1.Close();
-                fs2.Close();
-
-                return false;
-            }
-
-            do
-            {
-                file1byte = fs1.ReadByte();
-                file2byte = fs2.ReadByte();
-            }
-            while ((file1byte == file2byte) && (file1byte != -1));
-
-            fs1.Close();
-            fs2.Close();
-
-            return ((file1byte - file2byte) == 0);
+            return inputs;
         }
     }
 }
